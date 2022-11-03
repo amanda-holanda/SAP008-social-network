4fb7d67d9719348b85dc8b9c0ae4b5149dba7405
Amanda Holanda <amandaholanda_@hotmail.com>
2022-11-02 22:34:43 -0300

    Updates

--- pages/login/index.js
+++ pages/login/index.js
@@ -4,51 +4,48 @@ export default () => {
   const container = document.createElement('div');
   container.classList.add('wrapper-login');
   const template = `    
-    <section class="container-login"> 
-      <div class="container-login1">
-        <img src="./img/picsfem.png" class="logo-login">                  
+    <section class="container-login">      
+      <img src="./img/picsfem.png" class="logo-login">                  
 
-        <form class="form-login">
-          <div class="email-and-password-container">
-            <div class="iconmail"> 
+      <form class="form-login">
+        <div class="email-and-password-container">
+          <div class="iconmail"> 
             <img class="mail" src="img/icon-mail.png">      
             <label class="login-label" id="loginLabel" for="email">              
               <input id="txtEmail" type="email" name="email" class="input-login" placeholder="Email" required>
             </label>
-            </div> 
-            <div class="iconmail"> 
+          </div> 
+
+          <div class="iconmail"> 
             <img class="mail" src="img/icon-heart-lock.png">
             <label class="login-label" id="loginLabel" for="password">                
               <input id="txtPassword" type="password" name="password" class="input-login" placeholder="Senha" minlength="8" required>
             </label>              
-            </div>
-          </div>           
+          </div>
+        </div>           
               
-          <div class="signup-and-forgotpassword-container">              
-            <a href="#signup" type="button" class="signup-and-forgotpassword" style="text-decoration:none">Criar conta</a>
+        <div class="signup-and-forgotpassword-container">              
+          <a href="#signup" type="button" class="signup-and-forgotpassword" style="text-decoration:none">Criar conta</a>
                 
-            <a href="#" class="signup-and-forgotpassword">Esqueceu a senha?</a>
-          </div>   
+          <a href="#" class="signup-and-forgotpassword">Esqueceu sua senha?</a>
+        </div>   
 
-          <p class="txt-error hide" id="txtError"></p>   
+        <span class="txt-error hide" id="txtError"></span>
 
-          <button id="btnLogin" type="button" class="btn-login">Entrar</button>                  
+        <button id="btnLogin" type="button" class="btn-login">Entrar</button>                  
                                                   
-        </form>
-      </div>
-
-      <div class="container-login2">
-        <div class="line-container">
+      </form>
+      
+      <div class="line-container">
           <span class="line"></span>
           <span class="txt-line">ou</span>
           <span class="line"></span>        
-        </div>
+      </div>
               
-        <button id="btn-gmail" type="button" class="btn-google">
+      <button id="btn-gmail" type="button" class="btn-google">
           <img class="google-icon" src="./img/google-icon.png" alt="google-icon">
           Continue com google
-        </button>
-      </div>               
+      </button>                    
       
     </section>       
 
@@ -64,20 +61,21 @@ export default () => {
   const btnGmail = container.querySelector('#btn-gmail');
   const txtError = container.querySelector('#txtError');
 
-  btnLogin.addEventListener('click', () => {
-    loginEmailPassword(txtEmail.value, txtPassword.value)
+  btnLogin.addEventListener('click', async () => {
+    await loginEmailPassword(txtEmail.value, txtPassword.value)
       .then(() => {
         window.location.hash = '#feed';
       })
       .catch(() => {
+        window.location.hash = '#login';
         txtError.setAttribute('style', 'display: block');
         txtError.innerHTML = 'Usuário ou senha incorretos';
         txtPassword.focus();
       });
   });
 
-  btnGmail.addEventListener('click', () => {
-    signInGoogle()
+  btnGmail.addEventListener('click', async () => {
+    await signInGoogle()
       .then(() => {
         window.location.hash = '#feed';
       })
